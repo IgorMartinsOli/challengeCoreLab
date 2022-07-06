7e22943fea262a51c47e3e71b6f315076a497e28
Igor_ <igrmartoli@gmail.com>
2022-07-05 23:31:56 -0300

    alter port and install cors

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ var connection = require('./database/database');
 var Vehicle = require('./Vehicle/vechicle');
 var vechicleController = require('./Vehicle/vehicleController');
 var bodyParser = require('body-parser')
+var cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.json());
+app.use(cors())
 
 connection
     .authenticate()
@@ -19,6 +21,6 @@ connection
 
 app.use('/vehicles', vechicleController);
 
-app.listen(3000, () => {
-        console.log('listening on 3000');
+app.listen(3001, () => {
+        console.log('listening on 3001');
 });
