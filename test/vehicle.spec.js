daabac8f03eba711f849b272f246cca88d7f76d9
Igor_ <igrmartoli@gmail.com>
2022-07-06 22:48:57 -0300

    try test

--- test/vehicle.spec.js
+++ test/vehicle.spec.js
@@ -1,37 +1,19 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let mocha = require('mocha');
 var expect = chai.expect;
-const baseUrl = 'http://localhost:8008';
+var app = require('.././index')
 
 chai.use(chaiHttp);
 
-let vehicleTest = {
-    id: 1,
-    name: "Celta",
-    brand: "Fiat",
-    color: "green",
-    year: 2002,
-    board: "NOOB001"
-}
 
-describe('vehicles test', () => {
-    it('search all vehicles', (done) => {
-        chai.request(baseUrl)
-        .get('/vehicles')
-        .end((err, res) => {
-            expect(res).to.have.status(200);
-            done();
-        })
-    })
-
-    it('Update vehicle', () => {
-        chai.request(baseUrl)
-        .get('/vechicle/update')
-        .send(vehicleTest)
-        .end((err, res) => {
-            expect(err).to.have.status(200);
-            done();
+describe('Teste de Veiculos', () => {
+    describe('GET /vehicles', () => {
+        it('testando get vehicles' , () => {
+            chai.request('app')
+            .get('/vehicles')
+            .end((err, res) => {
+                
+            })
         })
     })
 })
