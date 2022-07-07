7b0c1f9750e2967fa668727c7d63bd7bcbc700a9
Igor_ <igrmartoli@gmail.com>
2022-07-06 22:48:51 -0300

    try test

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var vechicleController = require('./Vehicle/vehicleController');
 var bodyParser = require('body-parser')
 var cors = require('cors');
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 connection
     .authenticate()
@@ -21,6 +21,15 @@ connection
 
 app.use('/vehicles', vechicleController);
 
-app.listen(3001, () => {
-        console.log('listening on 3001');
+app.use(function (req, res, next){
+    let err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 });
+
+app.use(function (err, req, res, next){
+    return res.status(err.status || 500)
+    .json({error: err.message || "INTERNAL ERROR"});
+});
+
+module.exports = app;
